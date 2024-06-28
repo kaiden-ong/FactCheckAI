a661c4a4e2613ba117588e454e935597ed30399f
kaiden-ong <kaiden.ong000@gmail.com>
2024-06-28 00:54:46 -0700

    fix server file

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const parserRouter = require('./parser'); // import the parser router
 
 const apiRouter = require('./routes/api');
 
 const app = express();
-// const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(logger('dev'));
@@ -21,11 +19,6 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/api', apiRouter);
 
 app.use('/api', require('./routes/api'));
-app.use('/parser', parserRouter);
 
-// Server listening
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
 
 module.exports = app;
