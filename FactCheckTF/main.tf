b17cb609a43da5adf8aa4bcb78bea932cb45beb6
Kaiden Ong <kaiden.ong000@gmail.com>
2024-08-08 16:47:09 -0700

    Using terraform to deploy, running into python issues

---
+++ FactCheckTF/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "factcheck_rg" {
+  name     = "FactCheckAI-RG"
+  location = "West US 3"
+}
+
+resource "azurerm_service_plan" "factcheck_sp" {
+  name                = "FactCheckAI-SP"
+  resource_group_name = azurerm_resource_group.factcheck_rg.name
+  location            = azurerm_resource_group.factcheck_rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "factcheck_lwa" {
+  name                = "FactCheckAI"
+  resource_group_name = azurerm_resource_group.factcheck_rg.name
+  location            = azurerm_service_plan.factcheck_sp.location
+  service_plan_id     = azurerm_service_plan.factcheck_sp.id
+
+  site_config {
+    app_command_line = "npm start"
+    application_stack {
+      node_version = "20-lts"
+    }
+  }
+}
--- server.js
+++ server.js
@@ -8,16 +8,20 @@ const apiRouter = require('./routes/api');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api', apiRouter);
 
-app.use('/api', require('./routes/api'));
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
 
 module.exports = app;
